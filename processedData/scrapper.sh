246ca0ac2d5ffd110bab3cc7fc0186474999bb0d
Albert <me@albert.sh>
2018-06-30 17:52:38 +0200

    Change constants in order to have better defaults

--- processedData/scrapper.sh
+++ processedData/scrapper.sh
@@ -1,6 +1,5 @@
 touch nius.txt
-#for niu in {1000000..1999999}
-for niu in {1458626..1999999}
+for niu in {1000000..1999999}
 do
 	echo "Testing $niu..."
 	nom=$(curl -s http://deic-projectes.uab.cat/gamification/perfil.php?niu=$niu | egrep "[A-z]* [A-z]*, [A-z]*.*</h2>" | sed -E "s/\t*([A-z]*) ([A-z]*), ([A-z]*).*/\3 \1 \2/g")
