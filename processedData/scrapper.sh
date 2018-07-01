eaf42ac01af7d2e85da225b08045e01ef07c9a09
Albert <me@albert.sh>
2018-07-01 18:14:43 +0200

    Modify scrapper.sh so it only looks for public profiles

--- processedData/scrapper.sh
+++ processedData/scrapper.sh
@@ -1,14 +1,15 @@
 touch nius.txt
+index=$(curl -L -s http://deic-projectes.uab.cat/gamification/development/index.html)
 for niu in {1000000..1999999}
 do
-	echo "Testing $niu..."
-	nom=$(curl -s http://deic-projectes.uab.cat/gamification/perfil.php?niu=$niu | egrep "[A-z]* [A-z]*, [A-z]*.*</h2>" | sed -E "s/\t*([A-z]*) ([A-z]*), ([A-z]*).*/\3 \1 \2/g")
-	if [ -z "$nom" ]
+	echo -n "Testing $niu... "
+	niuIndex=$(curl -L -s http://deic-projectes.uab.cat/gamification/perfil.php?niu=$niu)
+	if [ "$index" == "$niuIndex" ]
 	then
 		echo "Failure"
 	else
 		echo "Success!"
-		echo $niu $nom >> nius.txt
+		echo $niu >> nius.txt
 	fi
-	sleep 5
+	sleep 1
 done
